071727d666673f08641475a09aee0fb865c51176
Jaime Marcelo Valasek <jaime.valasek@gmail.com>
2026-07-24 20:00:13 -0300

    test: bound node test concurrency

--- tests/package-bin.test.js
+++ tests/package-bin.test.js
@@ -8,3 +8,7 @@ test('package exposes both aios and aioson CLI bins', () => {
   assert.equal(pkg.bin.aios, 'bin/aioson.js');
   assert.equal(pkg.bin['aioson'], 'bin/aioson.js');
 });
+
+test('package bounds Node test concurrency for subprocess-heavy Windows runs', () => {
+  assert.equal(pkg.scripts.test, 'node --test --test-concurrency=8');
+});
